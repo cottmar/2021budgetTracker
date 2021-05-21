2cb49d77dea43dfcd4661903ea90cbb57f033d6c
cottmar <cara.ottmar@gmail.com>
2021-05-20 22:58:30 -0700

    saving

--- src/App.js
+++ src/App.js
@@ -26,10 +26,15 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('in app.js');
+    console.log(expenses);
+  }
+
 
   return (
     <div>
-    <NewExpense />
+    <NewExpense onAddExpense={addExpenseHandler}/>
     <Expenses items={expenses}/>
     </div>
   );
