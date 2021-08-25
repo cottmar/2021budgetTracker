a24a14c844f3f62aa68ff504ae30ebe245732712
cottmar <cara.ottmar@gmail.com>
2021-08-24 22:03:24 -0700

    added expenses filtering by year, but visuals not rendering yet.

--- src/components/NewExpense/ExpenseFilter.js
+++ src/components/NewExpense/ExpenseFilter.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpenseFilter.css';
 
-const ExpensesFilter = () => {
-    const pickYear = () => {
-        console.log("CARA")
-    }
+const ExpensesFilter = (props) => {
+
+  const pickYearHandler = (e) => {
+    const yearData = e.target.value
+    console.log('Picked year', yearData)
+    props.showYearHandler(yearData);
+  }
+
+
     
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={pickYear}>
+        <select onChange={pickYearHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
